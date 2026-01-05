24fa885e5de9f1432674511e656400d1ddc12edd
ChethumiGeorge <onelageg05@gmail.com>
2026-01-05 11:55:54 +0530

    favourite tset

---
+++ src/tests/favourites.test.js
@@ -0,0 +1,43 @@
+import { expect } from "vitest";
+
+test('adds property to favourites', () => {
+    const favourites =[];
+    const property = {
+        id: 'prop1',
+        location: 'Petts Wood Road, Petts Wood, Orpington BR5',
+        type: 'House',
+        price: 750000
+    };
+
+    const addFavourite = (fav, prop) => {
+        if (!fav.find(f => f.id === prop.id)){
+            return [...fav, prop];
+        }
+        return fav;
+    };
+
+    const result= addFavourite(favourites, property);
+    expect(result.length).toBe(1);
+    expect(result[0]).toEqual(property);
+});
+
+test('prevents duplicate favourite', () => {
+    const property ={
+        id: 'prop1',
+        location: 'Petts Wood Road, Petts Wood, Orpington BR5',
+        type: 'House',
+        price: 750000
+    };
+    const favourites = [property];
+
+    const addFavourite = (fav, prop) =>{
+        if(!fav.find(f => f.id === prop.id)){
+            return [...fav, prop];
+        }
+        return fav;
+    };
+
+    const result = addFavourite(favourites, property);
+    expect(result.length).toBe(1);
+    expect(result[0]).toEqual(property);
+});
