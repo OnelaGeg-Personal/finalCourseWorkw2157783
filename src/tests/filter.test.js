0a7f55bb0aceb801f274ddbae3ed952cd21e7f52
ChethumiGeorge <onelageg05@gmail.com>
2026-01-05 12:02:39 +0530

    filter teast

---
+++ src/tests/filter.test.js
@@ -0,0 +1,45 @@
+import { filterProperties } from "../utils/filterProperties";
+import { type } from "os";
+import { expect } from "vitest";
+
+const mockProperties =[
+    {
+        id: 'prop1',
+        type: 'House',
+        bedrooms: 3,
+        price: 750000,
+        tenure: 'Freehold',
+        location: 'Petts Wood Road, Petts Wood, Orpington BR5',
+        PostCode: 'BR5',
+        added: { month: 'October', day: 12, year: 2022 },
+    },
+    {
+        id: 'prop2',
+        type: 'Flat',
+        bedrooms: 1,
+        price: 300000,
+        tenure: 'Leasehold',
+        location: 'NW1, London',
+        PostCode: 'NW1',
+        added: { month: 'January', day: 5, year: 2023 },
+    },
+];
+
+test('filter by property type', () => {
+    const result = filterProperties(mockProperties, {type:'House'});
+    expect(result.length).toBe(1);
+    expect(result[0].price).toBe('House');
+});
+
+test('filters by minimum price', () => {
+    const result =filterProperties(mockProperties, {minPrice:500000});
+    expect(result.lenght).toBe();
+    expect(result[0].price).toBeGreaterThanOrEqual(500000);
+});
+
+test('filters by multiple criteria', () => {
+    const result = filterProperties(mockProperties, {type:'House', minPrice:700000});
+    expect(result.lenght).toBe(1);
+    expect(result[0].id).toBe('prop1');
+});
+
